6bef610ac7280d1499781c5f0a668d3ba8b6ad5a
DEV <dev@info.local>
2021-08-27 16:47:37 -0300

    wille

---
+++ Luiz Felipe Vera Cruz - curso c#/aula/exercicios/Calculadora-Peso-Altura-Sexo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Calculadora_Peso_Altura_Sexo
+{
+    class Program
+    {
+        private const string V = "";
+
+        static void Main(string[] args)
+        {
+            //Faça um programa que receba a idade, o peso, altura, e o sexo de 10 pessoas. Calcule e exiba
+            //a - total de homens
+            //b - Total de mulheres
+            //c - Média de idade dos homens
+            //d - média de idade das mulheres
+            //e - Quantidade de pessoas que estão acima do peso, abaixo do peso e com peso ideal
+
+            //IMC - PESO / ALTURA * ALTURA
+
+            int peso = 0;
+            int altura = 0;
+            string sexo = "";
+            int idade = 0;
+
+            Console.Write("Digite a sua idade: ");
+            idade = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o seu peso: ");
+            peso = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a sua altura: ");
+            altura = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o seu sexo (F - Feminino) ou (M - Masculino): ");
+            sexo = (Console.ReadLine());
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Luiz Felipe Vera Cruz - curso c#/aula/exercicios/calculadora idade/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace calculadora_idade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+            int contadorMaior = 0;
+            int contadormenor = 0;
+            int somaIdade = 0;
+
+
+            do{
+                Console.Write("Digite a idade: ");
+                int idade = int.Parse(Console.ReadLine());
+
+                if(idade >= 18){
+                    contadorMaior = contadorMaior++;
+
+                }else{
+                    contadormenor = contadormenor++;
+
+                }
+
+                somaIdade = somaIdade + idade;
+                contador = contador +1;
+
+            }while (contador < 10);
+
+            Console.WriteLine($"A média de idade de todos é: {somaIdade/contador}");
+
+
+        }
+    }
+}
